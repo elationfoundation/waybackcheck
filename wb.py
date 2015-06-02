ac7f1a8a6bd8c78c98657c2977339a3de9856596
seamus tuohy <s2e@seamustuohy.com>
2015-06-02 08:45:22 -0400

    cleaning up and adding exception handling to archive link.

--- wb.py
+++ wb.py
@@ -1,20 +1,37 @@
+# LICENSE
+# Original Copyright: Asad Dhamani
+# Modified slightly by: seamus tuohy
+# GNU GENERAL PUBLIC LICENSE - Version 2, June 1991
+# https://github.com/dhamaniasad/waybackcheck/tree/2697166c784f09678665245ea9eb3322b92ea88e
+
 # Import libraries
 
 from sys import argv as arg
 import urllib2 as urllib
 import json
 
-# Submit url to wayback machine
 
-inputurl = arg[1] # take in url as argument
-wbkurl = "http://web.archive.org/save/" # wayback machine submission url
-urllib.urlopen(wbkurl+inputurl) # Submit the url
+def get_archived_url(inputurl):
+    # Submit url to wayback machine
+    wbkurl = "http://web.archive.org/save/" # wayback machine submission url
+    try:
+        urllib.urlopen(wbkurl+inputurl) # Submit the url
+    except urllib.HTTPError as err:
+        return inputurl
 
-# Return latest snapshot url
+    # Return latest snapshot url
+    wbkav = "http://archive.org/wayback/available?url=" # wayback machine availability api url
+    wbcheck = urllib.urlopen(wbkav+inputurl) # open wayback availability url
+    wbcheckjson = json.load(wbcheck) # load json data
+    archived_snapshots = wbcheckjson['archived_snapshots']
+    latest_snapshot = archived_snapshots['closest']['url']
+    return latest_snapshot
 
-wbkav = "http://archive.org/wayback/available?url=" # wayback machine availability api url
-wbcheck = urllib.urlopen(wbkav+inputurl) # open wayback availability url
-wbcheckjson = json.load(wbcheck) # load json data
-archived_snapshots = wbcheckjson['archived_snapshots']
-latest_snapshot = archived_snapshots['closest']['url']
-print latest_snapshot
+def main():
+    inputurl = arg[1] # take in url as argument
+    archivedurl = get_archived_url(inputurl)
+    print archivedurl
+
+
+if __name__ == '__main__':
+    main()
